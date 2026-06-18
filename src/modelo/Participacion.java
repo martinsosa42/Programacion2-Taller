5ecc78ca04a0c70c01655ccb52d37d0898bc0886
Martin <sosamartin9910@gmail.com>
2026-06-18 18:21:11 -0300

    Metodos para informacion en proceso

--- src/modelo/Participacion.java
+++ src/modelo/Participacion.java
@@ -17,6 +17,50 @@ public class Participacion {
         this.seleccion = seleccion;
         this.partido = partido;
     }
+
+    public int cantidadGoles() {
+    int goles = 0;
+    if (this.partido != null && this.partido.getEventos() != null && this.seleccion != null) {
+        for (Evento e : this.partido.getEventos()) {
+            // Verificamos si el evento es un gol o penal convertido
+            if (e.getTipo() == TipoEvento.GOL || e.getTipo() == TipoEvento.PENAL_CONVERTIDO) {
+                // Verificamos si el jugador que hizo el gol pertenece a ESTA selección
+                if (this.seleccion.getJugadores().contains(e.getJugador())) {
+                    goles++;
+                }
+            }
+        }
+    }
+    return goles;
+    }
+
+    public int cantidadTarjAmarillas() {
+        int amarillas = 0;
+        if (this.partido != null && this.partido.getEventos() != null && this.seleccion != null) {
+            for (Evento e : this.partido.getEventos()) {
+                if (e.getTipo() == TipoEvento.TARJETA_AMARILLA) {
+                    if (this.seleccion.getJugadores().contains(e.getJugador())) {
+                        amarillas++;
+                    }
+                }
+            }
+        }
+        return amarillas;
+    }
+
+    public int cantidadTarjRojas() {
+        int rojas = 0;
+        if (this.partido != null && this.partido.getEventos() != null && this.seleccion != null) {
+            for (Evento e : this.partido.getEventos()) {
+                if (e.getTipo() == TipoEvento.TARJETA_ROJA || e.getTipo() == TipoEvento.DOBLE_AMARILLA) {
+                    if (this.seleccion.getJugadores().contains(e.getJugador())) {
+                        rojas++;
+                    }
+                }
+            }
+        }
+        return rojas;
+    }
     
     //Getters
     public boolean isEsLocal() {
--- src/modelo/ServicioMundial.java
+++ src/modelo/ServicioMundial.java
@@ -12,7 +12,7 @@ public class ServicioMundial {
        this.mundial = mundial; 
    }   
 
-   public List<Jugador> obtenerRankingGoleadores() {
+    public List<Jugador> obtenerRankingGoleadores() {
     List<Jugador> goleadores = new ArrayList<>();
 
     // 1. Controlamos que la lista de sedes del mundial no esté vacía
@@ -48,4 +48,174 @@ public class ServicioMundial {
 
     return goleadores;
    }
+
+   // 1.1 Tabla de Posiciones por Grupo (Ordenada por puntos y diferencia de gol)
+    public List<Seleccion> obtenerTablaPosiciones(Grupo grupo) {
+        if (grupo == null || grupo.getSelecciones() == null) {
+            return new ArrayList<>();
+        }
+        
+        List<Seleccion> clasificacion = new ArrayList<>(grupo.getSelecciones());
+        
+        // Ordenamos de mayor a menor puntaje utilizando el método del grupo
+        Collections.sort(clasificacion, new Comparator<Seleccion>() {
+            @Override
+            public int compare(Seleccion s1, Seleccion s2) {
+                int puntos1 = grupo.obtenerPuntos(s1);
+                int puntos2 = grupo.obtenerPuntos(s2);
+                
+                if (puntos1 != puntos2) {
+                    return Integer.compare(puntos2, puntos1);
+                }
+                // Criterio secundario: Diferencia de goles
+                return Integer.compare(calcularDiferenciaDeGol(s2, grupo), calcularDiferenciaDeGol(s1, grupo));
+            }
+        });
+        
+        return clasificacion;
+    }
+
+    private int calcularDiferenciaDeGol(Seleccion s, Grupo g) {
+        int golesAFavor = 0;
+        int golesEnContra = 0;
+
+        if (g.getFase() == null || g.getFase().getPartidos() == null){
+		    return 0;
+        }
+
+        for (Partido p : g.getFase().getPartidos()) {
+            if (p.getSeleccionLocal() != null && p.getSeleccionLocal().equals(s)) {
+                golesAFavor += p.getGolesLocal();
+                golesEnContra += p.getGolesVisitante();
+            } else if (p.getSeleccionVisitante() != null && p.getSeleccionVisitante().equals(s)) {
+                golesAFavor += p.getGolesVisitante();
+                golesEnContra += p.getGolesLocal();
+            }
+        }
+        return golesAFavor - golesEnContra;
+    }
+
+    public String obtenerResultadosPorSeleccion(Seleccion seleccion) {
+        if (seleccion == null) {
+            return "Selección no válida.";
+        }
+
+        String reporte = "TABLA DE RESULTADOS PARA: " + seleccion.getNombreFederacion() + "\n";
+    
+        // 1. Mostrar puntos si tiene un grupo asignado
+        if (seleccion.getGrupo() != null) {
+            int puntos = seleccion.getGrupo().obtenerPuntos(seleccion);
+            reporte += "- Fase de Grupos (" + seleccion.getGrupo().getIdentificacion() + "): " + puntos + " Puntos.\n";
+        }
+    
+        reporte += "- Partidos Jugados:\n";
+    
+        // 2. Recorrido en sus participaciones para listar los partidos
+        for (Participacion p : seleccion.getParticipaciones()) {
+            Partido partido = p.getPartido();
+        
+            reporte += "  * Instancia: " + partido.getFase() + " -> "
+                + partido.getSeleccionLocal().getNombreFederacion() + " " 
+                + partido.getGolesLocal() + " - "
+                + partido.getGolesVisitante() + " " 
+                + partido.getSeleccionVisitante().getNombreFederacion() + "\n";
+        }
+        return reporte;
+    }
+
+    public String generarInformeDisciplinarioPorSeleccion(List<Seleccion> selecciones) {
+        if (selecciones == null || selecciones.isEmpty()){
+            return "No hay selecciones registradas para el informe.";
+        }
+    
+        String reporte = " INFORME DISCIPLINARIO POR SELECCIÓN \n";
+    
+        for (Seleccion sel : selecciones) {
+            int totalAmarillas = 0;
+            int totalRojas = 0;
+    
+            for (Participacion p : sel.getParticipaciones()) {
+                totalAmarillas += p.cantidadTarjAmarillas();
+                totalRojas += p.cantidadTarjRojas();
+            }
+        
+            reporte += " Selección: " + sel.getNombreFederacion() + "\n"
+                + "Total Tarjetas Amarillas: " + totalAmarillas + "\n"
+                + "Total Tarjetas Rojas: " + totalRojas + "\n";
+            }
+        return reporte;
+    }
+
+    public String generarInformeDisciplinarioPorJugador(List<Jugador> jugadores) {
+        if (jugadores == null || jugadores.isEmpty()) {
+            return "No hay jugadores registrados para el informe.";
+        }
+    
+    String reporte = "INFORME DISCIPLINARIO INDIVIDUAL POR JUGADOR \n";
+    
+    for (Jugador j : jugadores) {
+        int amarillas = 0;
+        int rojas = 0;
+        
+        for (Evento e : j.getEventos()) {
+            if (e.getTipo() == TipoEvento.TARJETA_AMARILLA) {
+                amarillas++;
+            } else if (e.getTipo() == TipoEvento.TARJETA_ROJA || e.getTipo() == TipoEvento.DOBLE_AMARILLA) {
+                rojas++;
+            }
+        }
+
+        reporte += j.getNombre() + " (" + j.getPosicion() + ")\n"
+                    +"Tarjetas Amarillas: " + amarillas + "\n"
+                    +"Tarjetas Rojas: " + rojas + "\n";
+    }
+    return reporte;
+    }
+
+    public String generarFichaTecnicaPartido(Partido partido) {
+        if (partido == null){ 
+            return "Partido inexistente.";
+        }
+
+        String ficha = "FICHA TÉCNICA DEL PARTIDO\n"
+                 + "=========================================\n"
+                 + "Estadio: " + partido.getEstadio().getNombre() 
+                 + " (" + partido.getEstadio().getSede().getCiudad() + ")\n"
+                 + "Fecha: " + partido.getFecha() + " | Hora: " + partido.getHora() + "\n"
+                 + "Fase: " + partido.getFase() + "\n"
+                 + "-----------------------------------------\n"
+                 + "RESULTADO: " + partido.getSeleccionLocal().getNombreFederacion() + " " 
+                 + partido.getGolesLocal() + " - " + partido.getGolesVisitante() + " " 
+                 + partido.getSeleccionVisitante().getNombreFederacion() + "\n"
+                 + "-----------------------------------------\n"
+                 + "ALINEACIONES:\n";
+    
+        // Alineación Local
+        ficha += " Local (" + partido.getSeleccionLocal().getNombreFederacion() + "):\n";
+        for (Jugador j : partido.getSeleccionLocal().getJugadores()) {
+            ficha += "   [#" + j.getDorsal() + "] " + j.getNombre() + " (" + j.getPosicion() + ")\n";
+        }
+    
+        // Alineación Visitante
+        ficha += " Visitante (" + partido.getSeleccionVisitante().getNombreFederacion() + "):\n";
+        for (Jugador j : partido.getSeleccionVisitante().getJugadores()) {
+            ficha += "   [#" + j.getDorsal() + "] " + j.getNombre() + " (" + j.getPosicion() + ")\n";
+        }
+    
+        ficha += "-----------------------------------------\n"
+           + "EVENTOS:\n";
+    
+        // Cronología de eventos del partido
+        if (partido.getEventos().isEmpty()) {
+            ficha += " No se registraron eventos.\n";
+        } else {
+            for (Evento e : partido.getEventos()) {
+                ficha += " Minuto " + e.getMinuto() + " | " + e.getTipo() 
+                  + " -> " + e.getJugador().getNombre() + "\n";
+            }
+        }
+    return ficha;
+    }
+
+    
 }
